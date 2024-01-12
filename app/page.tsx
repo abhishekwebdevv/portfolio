51992316395fa3a2c079f7eea663279b5d0f81db
Abhishek Singh <abhishekdhakad61@gmail.com>
2024-01-13 03:38:10 +0530

    add hash from window object

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,89 @@
 export default function Home() {
-  return <></>;
+  return (
+    <div className="p-5 flex flex-col gap-y-5">
+      <div id="one" className="text-3xl">
+        <h2 className="font-bold">ONE</h2>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis quod
+        illo ullam et repellendus id molestiae odio asperiores voluptatum veniam
+        itaque explicabo recusandae natus tenetur enim tempore, ut dolorum
+        libero! Consectetur, explicabo. Animi natus architecto harum debitis
+        provident saepe explicabo commodi quasi, dicta accusamus quidem,
+        expedita libero placeat consectetur, corporis fugiat illo quam
+        perspiciatis. Ab earum autem dignissimos expedita aspernatur? Quibusdam
+        ab neque odio quos praesentium nam, suscipit temporibus accusamus
+        cupiditate impedit at atque? Atque eveniet blanditiis sed magnam, dolor
+        iure nulla doloribus similique aspernatur explicabo, nisi assumenda
+        veniam dignissimos. Dolor, repudiandae excepturi quidem quis fugit
+        placeat voluptates dolorem modi cumque. Explicabo facere optio excepturi
+        voluptatibus quia! Sunt ea facilis iusto hic velit doloribus explicabo
+        asperiores error impedit. Delectus, nisi? Impedit in blanditiis
+        doloribus cum eaque eius quis tenetur ab? Nulla porro minus, earum
+        veniam obcaecati voluptas maxime omnis dignissimos nemo eum illo velit
+        ea fuga harum nam impedit temporibus.
+      </div>
+
+      <div id="two" className="text-3xl">
+        <h2 className="font-bold">TWO</h2>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis quod
+        illo ullam et repellendus id molestiae odio asperiores voluptatum veniam
+        itaque explicabo recusandae natus tenetur enim tempore, ut dolorum
+        libero! Consectetur, explicabo. Animi natus architecto harum debitis
+        provident saepe explicabo commodi quasi, dicta accusamus quidem,
+        expedita libero placeat consectetur, corporis fugiat illo quam
+        perspiciatis. Ab earum autem dignissimos expedita aspernatur? Quibusdam
+        ab neque odio quos praesentium nam, suscipit temporibus accusamus
+        cupiditate impedit at atque? Atque eveniet blanditiis sed magnam, dolor
+        iure nulla doloribus similique aspernatur explicabo, nisi assumenda
+        veniam dignissimos. Dolor, repudiandae excepturi quidem quis fugit
+        placeat voluptates dolorem modi cumque. Explicabo facere optio excepturi
+        voluptatibus quia! Sunt ea facilis iusto hic velit doloribus explicabo
+        asperiores error impedit. Delectus, nisi? Impedit in blanditiis
+        doloribus cum eaque eius quis tenetur ab? Nulla porro minus, earum
+        veniam obcaecati voluptas maxime omnis dignissimos nemo eum illo velit
+        ea fuga harum nam impedit temporibus.
+      </div>
+
+      <div id="three" className="text-3xl">
+        <h2 className="font-bold">THREE</h2>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis quod
+        illo ullam et repellendus id molestiae odio asperiores voluptatum veniam
+        itaque explicabo recusandae natus tenetur enim tempore, ut dolorum
+        libero! Consectetur, explicabo. Animi natus architecto harum debitis
+        provident saepe explicabo commodi quasi, dicta accusamus quidem,
+        expedita libero placeat consectetur, corporis fugiat illo quam
+        perspiciatis. Ab earum autem dignissimos expedita aspernatur? Quibusdam
+        ab neque odio quos praesentium nam, suscipit temporibus accusamus
+        cupiditate impedit at atque? Atque eveniet blanditiis sed magnam, dolor
+        iure nulla doloribus similique aspernatur explicabo, nisi assumenda
+        veniam dignissimos. Dolor, repudiandae excepturi quidem quis fugit
+        placeat voluptates dolorem modi cumque. Explicabo facere optio excepturi
+        voluptatibus quia! Sunt ea facilis iusto hic velit doloribus explicabo
+        asperiores error impedit. Delectus, nisi? Impedit in blanditiis
+        doloribus cum eaque eius quis tenetur ab? Nulla porro minus, earum
+        veniam obcaecati voluptas maxime omnis dignissimos nemo eum illo velit
+        ea fuga harum nam impedit temporibus.
+      </div>
+
+      <div id="four" className="text-3xl">
+        <h2 className="font-bold">FOUR</h2>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis quod
+        illo ullam et repellendus id molestiae odio asperiores voluptatum veniam
+        itaque explicabo recusandae natus tenetur enim tempore, ut dolorum
+        libero! Consectetur, explicabo. Animi natus architecto harum debitis
+        provident saepe explicabo commodi quasi, dicta accusamus quidem,
+        expedita libero placeat consectetur, corporis fugiat illo quam
+        perspiciatis. Ab earum autem dignissimos expedita aspernatur? Quibusdam
+        ab neque odio quos praesentium nam, suscipit temporibus accusamus
+        cupiditate impedit at atque? Atque eveniet blanditiis sed magnam, dolor
+        iure nulla doloribus similique aspernatur explicabo, nisi assumenda
+        veniam dignissimos. Dolor, repudiandae excepturi quidem quis fugit
+        placeat voluptates dolorem modi cumque. Explicabo facere optio excepturi
+        voluptatibus quia! Sunt ea facilis iusto hic velit doloribus explicabo
+        asperiores error impedit. Delectus, nisi? Impedit in blanditiis
+        doloribus cum eaque eius quis tenetur ab? Nulla porro minus, earum
+        veniam obcaecati voluptas maxime omnis dignissimos nemo eum illo velit
+        ea fuga harum nam impedit temporibus.
+      </div>
+    </div>
+  );
 }
--- components/headers/Header.tsx
+++ components/headers/Header.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 
@@ -5,29 +6,38 @@ const menuList = [
   {
     id: 1,
     name: "Menu 1",
+    hash: "#one",
   },
   {
     id: 2,
     name: "Menu 2",
+    hash: "#two",
   },
   {
     id: 3,
     name: "Menu 3",
+    hash: "#three",
   },
   {
     id: 4,
     name: "Menu 4",
+    hash: "#four",
   },
 ];
 
 export function Header() {
+  console.log(window.location);
+  const { hash } = window.location;
+
   return (
     <header className="border-4 border-black rounded-3xl rounded-b-none flex items-center justify-center gap-x-4">
       {menuList.map((menuItem) => (
         <Link
-          href="/"
+          href={menuItem.hash}
           key={menuItem.id}
-          className="p-2 hover:bg-black hover:text-white"
+          className={`p-2 hover:bg-black hover:text-white ${
+            hash === menuItem.hash ? "bg-black text-white" : ""
+          }`}
         >
           {menuItem.name}
         </Link>
