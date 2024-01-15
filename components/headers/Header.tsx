e9657ba878bc281469ffb72a805f82ec743692d4
Abhishek Singh <abhishekdhakad61@gmail.com>
2024-01-15 21:38:12 +0530

    create custom hash logic

--- components/headers/Header.tsx
+++ components/headers/Header.tsx
@@ -1,46 +1,61 @@
 "use client";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import React from "react";
 
-const menuList = [
+//=================Types================//
+type HashTypes = "one" | "two" | "three" | "four";
+type MenuListItemType = {
+  id: number;
+  name: string;
+  hash: HashTypes;
+};
+//======================================//
+
+const menuList: MenuListItemType[] = [
   {
     id: 1,
     name: "Menu 1",
-    hash: "#one",
+    hash: "one",
   },
   {
     id: 2,
     name: "Menu 2",
-    hash: "#two",
+    hash: "two",
   },
   {
     id: 3,
     name: "Menu 3",
-    hash: "#three",
+    hash: "three",
   },
   {
     id: 4,
     name: "Menu 4",
-    hash: "#four",
+    hash: "four",
   },
 ];
 
 export function Header() {
-  console.log(window.location);
-  const { hash } = window.location;
+  const [currentHash, setCurrentHash] = useState<HashTypes>("one");
+
+  useEffect(() => {
+    document
+      .getElementById(currentHash)
+      ?.scrollIntoView({ behavior: "smooth" });
+  }, [currentHash]);
 
   return (
     <header className="border-4 border-black rounded-3xl rounded-b-none flex items-center justify-center gap-x-4">
       {menuList.map((menuItem) => (
-        <Link
-          href={menuItem.hash}
+        <button
           key={menuItem.id}
+          onClick={() => setCurrentHash(menuItem.hash)}
           className={`p-2 hover:bg-black hover:text-white ${
-            hash === menuItem.hash ? "bg-black text-white" : ""
+            currentHash === menuItem.hash ? "bg-black text-white" : ""
           }`}
         >
           {menuItem.name}
-        </Link>
+        </button>
       ))}
     </header>
   );
---
+++ hooks/useHash.ts
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+// import {useRouter} from "next/router"
+
+function useHash() {
+  // const router = useRouter()
+  // console.log(router)
+  // let hash = "";
+  // const getHash = () => {
+  //   console.log(window.location.hash);
+  //   hash = window.location.hash;
+  // };
+  // useEffect(() => {
+  //   window.addEventListener("hashchange", getHash);
+  //   return () => {
+  //     window.removeEventListener("hashchange", getHash);
+  //   };
+  // }, []);
+  // return hash;
+}
+
+export default useHash;
